1773c8a7981940fb8daa41f7ef0585bed98ee45f
DavidWaza <wazabanga00@gmail.com>
2022-02-02 14:10:01 +0100

    fixed a little padding

--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -3,14 +3,16 @@ import React from "react";
 import "./Topnav.css";
 import { Link} from "react-router-dom";
 
-
+const styles ={
+  marginTop:4
+}
 function Topnav() {
   return (
     <div>
       <nav className="nav">
         <div className="logo ">
           <Link to="/">
-            <h2>
+            <h2 style={styles}>
              <span className="waza"> Waza</span> <span className="eats">Eats</span>
             </h2>
           </Link>
